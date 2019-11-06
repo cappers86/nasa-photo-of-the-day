1791b1f1e18f63069b4d05b2bf51541adabbeed3
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-11-06 11:40:39 +0000

    Made axios request to Nasa api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import "./App.css";
 import axios from 'axios';
 
 function App() {
+
+  useEffect(() => {
+		console.log('NasaList component mounted');
+		
+		axios
+			.get('https://api.nasa.gov/planetary/apod?api_key=EQscKau3FR1jR3ws84unIpvf7x5PgLrXju8nlA4i')
+			.then(response => {
+				console.log('Nasa: ', response.data);
+				// Setting our movies data to state
+				
+			})
+			.catch(error => {
+				console.log('the data was not returned', error);
+			});
+	}, []);
+
   return (
     <div className="App">
       <p>
